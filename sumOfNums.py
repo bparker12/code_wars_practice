55af153e868dc4d8d31afbe5a02899b9e485cd31
bparker12 <49100894+bparker12@users.noreply.github.com>
2019-12-11 16:54:02 -0600

    new practice completed)

--- sumOfNums.py
+++ sumOfNums.py
@@ -10,3 +10,32 @@
 # get_sum(-1, 2) == 2  // -1 + 0 + 1 + 2 = 2
 
 
+def get_sum(a, b):
+    total_num = 0
+    if a > b:
+        num_range = list(range(b, a))
+        num_range.append(a)
+        # print(num_range)
+        for num in num_range:
+            total_num += num
+        print(total_num)
+    elif b > a:
+        num_range = list(range(a, b))
+        num_range.append(b)
+        # print(num_range)
+        for num in num_range:
+            total_num += num
+        print(total_num)
+    elif a == b:
+        print(a)
+
+
+get_sum(5, -1)
+get_sum(0, 4)
+
+get_sum(1, 0)
+get_sum(1, 2)
+get_sum(0, 1)
+get_sum(1, 1)
+get_sum(-1, 0)
+get_sum(-1, 2)
